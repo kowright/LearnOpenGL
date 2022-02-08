a6f668c358e35dcf58e8a4c1fecf934d8865b08c
Nick Wessing <nick@threespacelab.com>
2022-02-08 15:48:00 -0600

    Debug messenger

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -7,6 +7,67 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+static void gl_debug_messenger([[maybe_unused]] GLenum source, GLenum type,
+    [[maybe_unused]] GLuint id, GLenum severity,
+    [[maybe_unused]] GLsizei length,
+    const GLchar* message,
+    [[maybe_unused]] const void* user_param) {
+
+    const char* type_str = nullptr;
+    switch (type) {
+    case GL_DEBUG_TYPE_ERROR:
+        type_str = "Error";
+        break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        type_str = "Deprecated Behaviour";
+        break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        type_str = "Undefined Behaviour";
+        break;
+    case GL_DEBUG_TYPE_PORTABILITY:
+        type_str = "Portability";
+        break;
+    case GL_DEBUG_TYPE_PERFORMANCE:
+        type_str = "Performance";
+        break;
+    case GL_DEBUG_TYPE_MARKER:
+        type_str = "Marker";
+        break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:
+        type_str = "Push Group";
+        break;
+    case GL_DEBUG_TYPE_POP_GROUP:
+        type_str = "Pop Group";
+        break;
+    case GL_DEBUG_TYPE_OTHER:
+        type_str = "Other";
+        break;
+    }
+
+    const char* severity_str = nullptr;
+    switch (severity) {
+
+    case GL_DEBUG_SEVERITY_HIGH:
+        severity_str = "High";
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        severity_str = "Medium";
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        severity_str = "Low";
+        break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        severity_str = "Notification";
+        break;
+    }
+
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION) {
+        return;
+    }
+
+    printf("GL: [%s] [%s] %s\n", type_str, severity_str, message);
+}
+
 int main() {
 	printf("Hello, World!\n");
     glm::vec3 a{ 1.0, 0.0, 2.0 };
@@ -25,9 +86,16 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
+    }
+
+    if (GLAD_GL_KHR_debug) {
+        glDebugMessageCallback(gl_debug_messenger, nullptr);
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
     }
 
     while (!glfwWindowShouldClose(window)) {
