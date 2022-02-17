810fdde4fc63609595d8b628e814c94e99f92306
Kortney Wright <71905039+kowright@users.noreply.github.com>
2022-02-17 09:27:52 -0500

    multiple spinning 3D cubes

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -13,7 +13,10 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-float mixValue = 0.2f;
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 
 int main() {
 	printf("Hello, World!\n");
@@ -30,7 +33,7 @@ int main() {
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //for Mac OS X
 
     // glfw window creation
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -46,79 +49,80 @@ int main() {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    /*
 
-    //vertex shader
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //replacing original code of the shader object
-    glCompileShader(vertexShader);
-
-    //checking if vertex shader complied
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\\n" << infoLog << std::endl;
-    }
-
-    //fragment shader
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    //checking if fragment shader complied
-    int  successX;
-    char infoLogX[512];
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successX);
-    if (!successX)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogX);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\\n" << infoLogX << std::endl;
-    }
-
-    //shader program
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram); //attaches shaders to the program
-
-    //checking if program failed
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader); //both are in program now; not needed anymore
-    glDeleteShader(fragmentShader);
-    */
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST); //disabled by default 
+    
 
     // build and compile our shader zprogram
     Shader ourShader("../../Assets/3.3.shader.vs", "../../Assets/3.3.shader.fs"); //text files of vertex and fragment shaders 
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
-        // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+        //position          //texture coords
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    unsigned int indices[] = {
-      0, 1, 3, // first triangle
-      1, 2, 3  // second triangle
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO); //create VAO
     glGenBuffers(1, &VBO); //create VBO
-    glGenBuffers(1, &EBO); //create VBO 
+  
   
 
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
@@ -127,38 +131,29 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);//only one single buffer can be tied to each buffer type
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //copying vertices array into a buffer for OpenGL to use -> VBO
 
+    /*
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // -> EBO
-
+    */
 
 
     //where position attribute is in VBO
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); //how OpenGL should interpret VBO vertices. 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0); //how OpenGL should interpret VBO vertices. 
     glEnableVertexAttribArray(0); //by default is disabled
 
-    //where color attribute is in VBO
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); //1 is for this attribute being the second attribute in a VAO?? 
+    //where texture coord attribute is in VBO
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); //1 is for this attribute being the second attribute in a VAO
                        //(VAO attribute index, # of byte size of this attribute, data type of this attribute, normalize this?, byte offset between start of attribute to start of next iteration of this attribute (stride), where attribute first starts in array)
     glEnableVertexAttribArray(1); //same as above first argument
-
-    //where texture coordinates attribute is in VBO
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); 
-                       //(VAO attribute index,  # of byte size of this attribute, data type of this attribute, normalize this?, byte offset between start of attribute to start of next iteration of this attribute (stride), where attribute first starts in array)
-    glEnableVertexAttribArray(2); //same as above first argument
-
-
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind VBO to this buffer type since VBO is configured now
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0); //unbind VAO but not necessary since you always need to specificy the VAO to bind it
 
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
 
 
     // load and create a texture
@@ -220,8 +215,6 @@ int main() {
     ourShader.setInt("texture2", 1);
 
 
-
-
     while (!glfwWindowShouldClose(window)) {//render loop
         //input
         processInput(window);
@@ -252,9 +245,9 @@ int main() {
 
         //model matrix (local to world)
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-        //view matrix (world to view)
+        //view matrix (world to view) "camera"
         glm::mat4 view = glm::mat4(1.0f);
         // note that we're translating the scene in the reverse direction of where we want to move (moving camera backwards is same as moving entire scene forwards)
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -279,15 +272,7 @@ int main() {
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); //if this is above translation- rotates around the the center of screen. if this is below translate- picture in corner and rotating around its center
        */
         
-        /*
-        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0)); //rotate around z axis (a unit vector)
-        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));*/
-
-        // set the texture mix value in the shader
-        ourShader.setFloat("mixValue", mixValue);
-
     
-
 
         /*
         unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
@@ -295,7 +280,19 @@ int main() {
         //location of uniform, how many matrices, transfose matrix?, matrix data but GLM doesn't do it in a way OpenGL likes, so have to use value ptr
         */
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+
+            float angle = glfwGetTime() * 25.0f;
+
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+            
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
 
         //check and call events and swap buffers
@@ -317,24 +314,6 @@ void processInput(GLFWwindow* window)
     {
         glfwSetWindowShouldClose(window, true);
     }
-
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    {
-        mixValue += 0.01f; // change this value accordingly (might be too slow or too fast based on system hardware)
-        std::cout << "mix up" << std::endl;
-        if (mixValue >= 1.0f) {
-            mixValue = 1.0f;
-        }
-            
-    }
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    {
-        mixValue -= 0.01f; // change this value accordingly (might be too slow or too fast based on system hardware)
-        if (mixValue <= 0.0f) {
-            mixValue = 0.0f;
-        }
-    }
-
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
