9b13b346197950f5083d873091db4e52939afa17
Kortney Wright <71905039+kowright@users.noreply.github.com>
2022-02-17 08:58:31 -0500

    first 3D view

--- Assets/3.3.shader.vs
+++ Assets/3.3.shader.vs
@@ -8,9 +8,14 @@ out vec2 TexCoord;
 
 uniform float xOffset; 
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-   gl_Position = vec4(aPos, 1.0);
+ 
    ourColor = aColor;
    TexCoord = aTexCoord;
+   gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -22,12 +22,14 @@ int main() {
 
     printf("a + b = %f\n", glm::length(a + b));
 
+    // glfw: initialize and configure
     glfwInit(); //initializes GLFW library
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //setting hints for next create window call
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //making sure version 3.3 is used
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //for Mac OS X
 
+    // glfw window creation
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -39,7 +41,7 @@ int main() {
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //whenever window size changes
 
-
+    // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { //defines correct function depending on OS we are compliling for
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
@@ -95,9 +97,10 @@ int main() {
     glDeleteShader(fragmentShader);
     */
 
+    // build and compile our shader zprogram
     Shader ourShader("../../Assets/3.3.shader.vs", "../../Assets/3.3.shader.fs"); //text files of vertex and fragment shaders 
 
-    //set up vertex data
+    // set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
         // positions          // colors           // texture coords
          0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
@@ -156,9 +159,11 @@ int main() {
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    //texture setup 
+
+
+    // load and create a texture
     unsigned int texture1, texture2;
-    glGenTextures(1, &texture1); //create texture
+    glGenTextures(1, &texture1); //create texture 1 
 
     glBindTexture(GL_TEXTURE_2D, texture1); //supposed to active default texture first before binding but default texture is always activated so when bind texture is called, it is assigned to the activated texture0
     // set the texture wrapping/filtering options (on the currently bound texture object)
@@ -184,7 +189,7 @@ int main() {
     stbi_image_free(data);
 
 
-    glGenTextures(1, &texture2); //create texture
+    glGenTextures(1, &texture2); //create texture 2
 
     glBindTexture(GL_TEXTURE_2D, texture2); //supposed to active default texture first before binding but default texture is always activated so when bind texture is called, it is assigned to the activated texture0
     // set the texture wrapping/filtering options (on the currently bound texture object)
@@ -225,7 +230,6 @@ int main() {
         glClearColor(0.2, 0.1, 0.3, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //which buffer we would like to clear
 
-     
     
         /*
         // update the uniform color
@@ -241,11 +245,55 @@ int main() {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
+        // activate shader
+        ourShader.use();
+
+        //create transformations
+
+        //model matrix (local to world)
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+        //view matrix (world to view)
+        glm::mat4 view = glm::mat4(1.0f);
+        // note that we're translating the scene in the reverse direction of where we want to move (moving camera backwards is same as moving entire scene forwards)
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        //projection matrix (view to clip) 
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        ourShader.setMat4("projection", projection);
+
+        /*
+        glm::mat4 trans = glm::mat4(1.0f); //identity matrix must be made first or else who matrix will be 0 and any operations on it will be 0
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f)); //even though rotations happen before translations, translations should go first to ensure operations happen how you expect 
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); //if this is above translation- rotates around the the center of screen. if this is below translate- picture in corner and rotating around its center
+       */
+        
+        /*
+        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0)); //rotate around z axis (a unit vector)
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));*/
+
         // set the texture mix value in the shader
         ourShader.setFloat("mixValue", mixValue);
 
-        // render container
-        ourShader.use();
+    
+
+
+        /*
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        //location of uniform, how many matrices, transfose matrix?, matrix data but GLM doesn't do it in a way OpenGL likes, so have to use value ptr
+        */
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
